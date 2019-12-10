9071bed3d2bf9b8b474bc2f75058ab33f7c3ddc3
chriswasser <christian_wassermann@web.de>
2019-12-10 19:43:02 +0100

    Add cleaned up version of Day10 without tests

--- Day10/main.py
+++ Day10/main.py
@@ -1,11 +1,68 @@
 #!/usr/bin/env python3
 
 import fileinput
-from math import atan2, sqrt, pi
-from collections import namedtuple, defaultdict
+from math import atan2, sqrt, pi, inf
+from collections import namedtuple, defaultdict, OrderedDict
 
 
 Point = namedtuple('Point', field_names=['x', 'y'])
+
+
+def parse_asteroids(string):
+    return [
+        Point(x, y)
+        for y, line in enumerate(string.split('\n'))
+        for x, char in enumerate(line)
+        if char == '#'
+    ]
+
+
+def max_visibility(asteroids):
+    return max(
+        {
+            station: {
+                atan2(station.y - asteroid.y, station.x - asteroid.x)
+                for asteroid in asteroids
+                if asteroid != station
+            } for station in asteroids
+        }.items(),
+        key=lambda item: len(item[1])
+    )
+
+
+def euclidean_distance(a, b):
+    return sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
+
+
+def asteroids_by_angle(asteroids, station):
+    lines_of_sight = defaultdict(list)
+    for asteroid in asteroids:
+        if asteroid == station:
+            continue
+        angle = atan2(station.y - asteroid.y, station.x - asteroid.x)
+        lines_of_sight[angle].append(asteroid)
+    return lines_of_sight
+
+
+def reorder(lines_of_sight, station):
+    lines_of_sight = {
+        (angle * 180 / pi + 270) % 360: sorted(
+            asteroids,
+            key=lambda asteroid: euclidean_distance(station, asteroid),
+            reverse=True
+        ) for angle, asteroids in lines_of_sight.items()
+    }
+    return OrderedDict(sorted(lines_of_sight.items()))
+
+
+def vaporized_asteroids(lines_of_sight, n=inf):
+    vaporized = []
+    while len(vaporized) < n and all(lines_of_sight.values()):
+        for angle in lines_of_sight.keys():
+            if not lines_of_sight[angle]:
+                continue
+            vaporized.append(lines_of_sight[angle].pop())
+    return vaporized
 
 
 def test_task1():
@@ -14,22 +71,10 @@ def test_task1():
 
 
 def solve_task1():
-    asteroids = []
-    for y, line in enumerate(fileinput.input()):
-        for x, char in enumerate(line.rstrip('\n')):
-            if char == '#':
-                asteroids.append(Point(x, y))
-
-    max_visible = -1
-    for station in asteroids:
-        visible = {
-            atan2(station.y - asteroid.y, station.x - asteroid.x)
-            for asteroid in asteroids
-            if asteroid != station
-        }
-        if len(visible) > max_visible:
-            max_visible, max_station = len(visible), station
-    print(f'answer to task 1: {max_visible} @ {max_station}')
+    string = ''.join(fileinput.input())
+    asteroids = parse_asteroids(string)
+    maximum = max_visibility(asteroids)
+    print(f'answer to task 1: {len(maximum[1])}')
 
 
 def test_task2():
@@ -38,46 +83,13 @@ def test_task2():
 
 
 def solve_task2():
-    asteroids = []
-    for y, line in enumerate(fileinput.input()):
-        for x, char in enumerate(line):
-            if char == '#':
-                asteroids.append(Point(x, y))
-
-    station = Point(x=22, y=28)
-
-    angles = defaultdict(list)
-    for asteroid in asteroids:
-        if asteroid == station:
-            continue
-        angle = atan2(station.y - asteroid.y, station.x - asteroid.x)
-        dist = sqrt((station.x - asteroid.x) ** 2 + (station.y - asteroid.y) ** 2)
-        angles[angle].append((dist, asteroid))
-
-    for key in angles.keys():
-        angles[key] = sorted(angles[key], key=lambda x: x[0])
-
-    vaporized = []
-    while angles.keys():
-        for key in sorted(angles.keys(), key=lambda x: (x * 180 / pi + 270) % 360):
-            vaporized.append(angles[key][0])
-            del angles[key][0]
-            if not angles[key]:
-                del angles[key]
-
-    # 2404 too high
-    print(vaporized[0])
-    print(vaporized[1])
-    print(vaporized[2])
-    print(vaporized[9])
-    print(vaporized[19])
-    print(vaporized[49])
-    print(vaporized[99])
-    print(vaporized[198])
-    print(vaporized[199])
-    print(vaporized[200])
-    print(vaporized[298])
-    print(f'answer to task 2: {vaporized[199][1].x * 100 + vaporized[199][1].y}')
+    string = ''.join(fileinput.input())
+    asteroids = parse_asteroids(string)
+    station = max_visibility(asteroids)[0]
+    lines_of_sight = asteroids_by_angle(asteroids, station)
+    lines_of_sight = reorder(lines_of_sight, station)
+    asteroid = vaporized_asteroids(lines_of_sight, n=200)[199]
+    print(f'answer to task 2: {asteroid.x * 100 + asteroid.y}')
 
 
 def main():
